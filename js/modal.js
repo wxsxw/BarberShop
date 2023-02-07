8828835fd5a16dc76551f1ee624c6531f01b74ce
Svetlana Zakharchenko <wxsxw2011@gmail.com>
2023-02-07 21:53:45 +0200

    js-btn4

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,20 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelectorAll("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
   };
+  // for (let i = 0; i < refs.openModalBtn.length; i++) {
+  //   refs.openModalBtn[i].addEventListener("click", toggleModal);
+  // }
+  refs.openModalBtn.forEach((elem) => {
+    elem.addEventListener("click", toggleModal);
+  });
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  // refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+    refs.modal.classList.toggle("is-hidden");
   }
 })();
